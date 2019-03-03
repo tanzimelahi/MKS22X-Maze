7853b1ec6733a330251d5d012b5f5e2e86b31a03
tanzimelahi <telahi00@stuy.edu>
2019-03-03 13:59:55 -0500

    solve done, now testing

--- Maze.java
+++ Maze.java
@@ -7,21 +7,9 @@ import java.util.Arrays;
 public class Maze{
 
     private char[][]maze;
+    private int[][]moves;
     private boolean animate;//false by default
 
-    /*Constructor loads a maze text file, and sets animate to false by default.
-      When the file is not found then:
-         throw a FileNotFoundException
-
-      You may assume the file contains a rectangular ascii maze, made with the following 4 characters:
-      '#' - Walls - locations that cannot be moved onto
-      ' ' - Empty Space - locations that can be moved onto
-      'E' - the location of the goal (exactly 1 per file)
-      'S' - the location of the start(exactly 1 per file)
-
-      You ma also assume the maze has a border of '#' around the edges.
-      So you don't have to check for out of bounds!
-    */
     public Maze(String filename) throws FileNotFoundException{
     	animate=false;
         File text = new File(filename);
@@ -39,8 +27,15 @@ public class Maze{
         	   maze[i][j]=line.charAt(i*size+j);
            }
         }
-        //System.out.println(Arrays.deepToString(maze));
-
+        moves=new int[4][2];
+        moves[0][0]=0;
+        moves[0][1]=1;
+        moves[1][0]=0;
+        moves[1][1]=-1;
+        moves[2][0]=1;
+        moves[2][1]=0;
+        moves[3][0]=-1;
+        moves[3][1]=0;
     }
 
     private void wait(int millis){
@@ -60,13 +55,6 @@ public class Maze{
         System.out.println("\033[2J\033[1;1H");
     }
 
-
-
-
-
-   /*Return the string that represents the maze.
-     It should look like the text file with some characters replaced.
-    */
     public String toString(){
             String result="";
             for(int i=0;i<maze.length;i++) {
@@ -79,33 +67,30 @@ public class Maze{
     }
 
 
-    /*Wrapper Solve Function returns the helper function
-      Note the helper function has the same name, but different parameters.
-      Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
-    */
- //   public int solve(){
-            //find the location of the S.
 
-            //erase the S
+   public int solve(){
+        int []result=find();
+        maze[result[0]][result[1]]=' ';
+        return solve(result[0],result[1],0,true);
 
-            //and start solving at the location of the s.
-            //return solve(???,???);
-   // }
+   }
 
-    /*
-      Recursive Solve function:
 
-      A solved maze has a path marked with '@' from S to E.
 
-      Returns the number of @ symbols from S to E when the maze is solved,
-      Returns -1 when the maze has no solution.
-
-      Postcondition:
-        The S is replaced with '@' but the 'E' is not.
-        All visited spots that were not part of the solution are changed to '.'
-        All visited spots that are part of the solution are changed to '@'
-    */
-    private int solve(int row, int col){ //you can add more parameters since this is private
+    private int[] find(){
+      int[]result=new int[2];
+      for(int i=0;i<maze.length;i++){
+        for(int j=0;j<maze[0].length;j++){
+          if(maze[i][j]=='S'){
+            result[0]=i;
+            result[1]=j;
+            return result;
+          }
+        }
+      }
+      return result;
+    }
+    private int solve(int row,int col,int count,boolean go){ //you can add more parameters since this is private
 
         //automatic animation! You are welcome.
         if(animate){
@@ -113,8 +98,26 @@ public class Maze{
             System.out.println(this);
             wait(20);
         }
-
-        //COMPLETE SOLVE
+        if(maze[row][col]=='E'){
+          return count;
+        }
+        else if(maze[row][col]=='#'|| maze[row][col]=='@'){
+          go=false;
+          return -1;
+        }
+        else if(maze[row][col]==' '){
+          go=true;
+        }
+      if(go==true){
+        maze[row][col]='@';
+        for(int i=0;i<moves.length;i++){
+          int result=solve(row+moves[i][0],col+moves[i][1],count+1,true);
+           if(result>-1){
+             return result;
+           }
+        }
+      }
+  maze[row][col]='.';
         return -1; //so it compiles
     }
     public static void main(String[]args) {
@@ -122,6 +125,8 @@ public class Maze{
     	Maze mazes=new Maze("data.txt");
 
     	System.out.println(mazes);
+      mazes.solve();
+      System.out.println(mazes);
     	}
     	catch(FileNotFoundException e) {
     		System.out.println("yeah");
